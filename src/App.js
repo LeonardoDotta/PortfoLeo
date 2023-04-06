9f9ff96c4d90e611dc2fd53da81563dc3cd5d7c4
LeonardoDotta <ldotta918@gmail.com>
2023-04-06 19:37:05 -0300

    início

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Container from './components/layout/Container'
+import Header from './components/layout/Header'
+import Home from './components/pages/Home/Home'
+import Footer from './components/layout/Footer'
+import AcademicEducation from './components/pages/AcademicEducation/AcademicEducation'
+import Goal from './components/pages/Goal/Goal'
+import SkillsAndKnowledge from './components/pages/SkillsAndKnowledge/SkillsAndKnowledge'
+import Experience from './components/pages/Experience/Experience'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Header />
+      <Container>
+      <Routes>
+        <Route element={<Home />} exact path='/'>
+        </Route>
+      </Routes>
+      <Routes>
+        <Route element={<AcademicEducation />} exact path='/academic-education'>
+        </Route>
+      </Routes>
+      <Routes>
+        <Route element={<Goal />} exact path='/goal'>
+        </Route>
+      </Routes>
+      <Routes>
+        <Route element={<SkillsAndKnowledge />} exact path='/skills-and-knowledge'>
+        </Route>
+      </Routes>
+      <Routes>
+        <Route element={<Experience />} exact path='/experience'>
+        </Route>
+      </Routes>
+      </Container>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/components/pages/AcademicEducation/AcademicEducation.js
@@ -0,0 +1,5 @@
+function AcademicEducation () {
+    return 
+}
+
+export default AcademicEducation
